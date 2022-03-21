1a0e21b399eca1cc2abf72a85ffbca15acbf86d6
viktor <viktor.fijacko@hs-duesseldorf.de>
2022-03-21 15:09:36 +0100

    initial commit

---
+++ count_lines.sh
@@ -0,0 +1,8 @@
+# Small script to cound lines in tsv files
+# Viktor FIjacko viktor.fijacko@hs-duesseldorf.de
+# 2022-03-31
+
+echo "Counting lines in all tsv files"
+wc -l *tsv
+echo "Done! Have a nice Day" 
+
